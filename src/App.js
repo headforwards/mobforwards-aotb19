f39b88b0afe21ccd8f0d2c1389f9c0b04577ba8a
Headforwards <headforwards@Headforwardss-iMac.local>
2019-07-11 16:31:46 +0100

    Added speakers list

--- src/App.js
+++ src/App.js
@@ -5,18 +5,30 @@ import dataService from "./services/dataService";
 
 class App extends React.Component {
   state = {
-    rooms: []
+    rooms: [],
+    speakers: [],
+    events: []
   };
+
   componentDidMount() {
     dataService.getRooms().on("value", snapshot => {
-      const roomstuff = snapshot.val().rooms;
-      this.setState({ rooms: roomstuff });
+      const db = snapshot.val();
+      this.setState({
+        rooms: db.rooms,
+        speakers: db.speakers,
+        events: db.events
+      });
     });
   }
   render() {
+    var speakers = this.state.speakers.map(s => (
+      <p>{s.person_display_last_name}</p>
+    ));
+
     return (
       <div className="App">
         <List rooms={this.state.rooms} />
+        <div>{speakers}</div>
       </div>
     );
   }
