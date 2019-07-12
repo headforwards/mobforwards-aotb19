ca29e9cd5cfd83d82b5a053711e76c3740bc5050
Headforwards <headforwards@Headforwardss-iMac.local>
2019-07-12 13:26:19 +0100

    Display Speaker Name

--- src/components/Room.js
+++ src/components/Room.js
@@ -5,32 +5,50 @@ import moment from "moment";
 export default class Room extends Component {
   constructor() {
     super();
-    this.state = { events: [] };
+    this.state = { events: [], speakers: [] };
   }
 
   async componentDidMount() {
     let data = await dataService.getData();
     this.setState({
-      events: data.talks
+      events: data.talks,
+      speakers: data.speakers
     });
   }
 
   render() {
     const { params } = this.props.match;
-    const { events } = this.state;
+    const { events, speakers } = this.state;
 
     const filteredEvents = events
-      .filter(event => event.location_name === params.name)
-      .map(event => {
-        const displayDate = moment(event.start_datetime).format("HH:mm");
+      .filter(event => {
+        const endDate = moment(event.end_datetime);
+        return event.location_name === params.name && endDate > moment();
+      })
+      .sort((a, b) => {
+        const startDateA = moment(a.start_datetime);
+        const startDateB = moment(b.start_datetime);
 
+        return startDateA.valueOf() - startDateB.valueOf();
+      })
+      .map(event => {
+        const start = moment(event.start_datetime).format("HH:mm");
+        const end = moment(event.end_datetime).format("HH:mm");
+        const presenterId = event.presenter_oids[0];
+        const speakername = speakers.find(
+          speaker => speaker.oid === presenterId
+        ).person_display_last_name;
         return (
-          <p data-talk key={event.name}>
+          <div data-talk key={event.name}>
             <span className="pr-2" data-talk-time>
-              <strong>{displayDate}</strong>
+              <h3>
+                {start} - {end}
+              </h3>
             </span>
-            {event.name}
-          </p>
+            <h4>{event.name}</h4>
+            <h5 data-speaker-name>{speakername}</h5>
+            <p>{event.description}</p>
+          </div>
         );
       });
 
--- src/components/Room.test.js
+++ src/components/Room.test.js
@@ -1,22 +1,41 @@
 import React from "react";
 import { mount, shallow } from "enzyme";
 import Room from "./Room";
+import { async } from "q";
 
 jest.mock("../services/dataService", () => {
   return {
     getData: async () => ({
+      speakers: [
+        {
+          person_display_last_name: "Andrew Nesling",
+          oid: "IJrac4kPt8"
+        }
+      ],
       talks: [
         {
           location_name: "room1",
           name: "talk 1",
-          start_datetime: "2019-07-11T10:30:00Z",
-          end_datetime: "2019-07-11T11:15:00Z"
+          start_datetime: "2019-07-12T21:30:00Z",
+          end_datetime: "2019-07-12T22:30:00Z",
+          description: "Description 1",
+          presenter_oids: ["IJrac4kPt8"]
         },
         {
           location_name: "room1",
           name: "talk 2",
-          start_datetime: "2019-07-11T10:30:00Z",
-          end_datetime: "2019-07-11T11:15:00Z"
+          start_datetime: "2019-07-12T20:30:00Z",
+          end_datetime: "2019-07-12T21:30:00Z",
+          description: "Description 2",
+          presenter_oids: ["IJrac4kPt8"]
+        },
+        {
+          location_name: "room1",
+          name: "talk 3",
+          start_datetime: "2019-07-12T10:30:00Z",
+          end_datetime: "2019-07-12T11:15:00Z",
+          description: "Description 3",
+          presenter_oids: ["IJrac4kPt8"]
         }
       ]
     })
@@ -51,6 +70,38 @@ describe("room test suite", () => {
         .find("[data-talk-time]")
         .at(0)
         .text()
-    ).toBe("11:30");
+    ).toBe("21:30 - 22:30");
+  });
+
+  it("orders the time in ascending order", async () => {
+    await component.update();
+    let rooms = component.find("h3");
+    expect(rooms.at(0).text()).toBe("21:30 - 22:30");
+  });
+
+  it("does not show times in the past", async () => {
+    await component.update();
+    let rooms = component.find("h3");
+    expect(rooms).toHaveLength(2);
+  });
+
+  it("shows the talk description", async () => {
+    await component.update();
+    expect(
+      component
+        .find("p")
+        .at(0)
+        .text()
+    ).toBe("Description 2");
+  });
+
+  it("shows the last name of the speaker", async () => {
+    await component.update();
+    expect(
+      component
+        .find("[data-speaker-name]")
+        .at(0)
+        .text()
+    ).toBe("Andrew Nesling");
   });
 });
